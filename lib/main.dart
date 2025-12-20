5a8253c4df8e616260f5320734a09d6050b142fe
Onotchi <wonotchi@hotmail.com>
2025-12-20 19:15:55 +0900

    デバッグバナーを消す
    デバッグバナーを消す

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'VRM Viewer',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
